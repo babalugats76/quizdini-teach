8051adefef5be3ba8b628a2888c09a7eaeec5d77
James Colestock <james@colestock.com>
2021-02-09 12:00:50 -0700

    converted layout to sfc, etc.

---
+++ client/src/layouts/Layout.vue
@@ -0,0 +1,28 @@
+<template>
+  <slot />
+</template>
+
+<script>
+export default {
+  name: "Layout",
+  inheritAttrs: false,
+  props: {
+    name: {
+      type: String,
+      default: "div",
+      required: true,
+    },
+  },
+  emits: ["update:layout"],
+  created() {
+    this.$emit("update:layout", this.name);
+  },
+};
+</script>
+
+<style lang="scss" scoped></style>
+
+// export default { // name: "Layout", // inheritAttrs: false, // props: { //
+name: { // type: String, // default: "div", // required: true, // }, // }, //
+emits: ["update:layout"], // setup(props, { emit, slots }) { //
+emit("update:layout", props.name); // return () => slots.default(); // }, // };
