43994facd5138525ae7bb833fb1aad7d06762b5c
James Colestock <james@colestock.com>
2021-03-30 15:49:15 -0600

    PingChart and some grid component poc work

--- client/src/components/PingChart.vue
+++ client/src/components/PingChart.vue
@@ -13,7 +13,7 @@ export default {
   props: {
     createDate: {
       type: String,
-      default: () => Date.now(),
+      //   default: () => Date.now(),
       required: true,
     },
     pings: {
@@ -27,141 +27,139 @@ export default {
     const canvasRef = ref();
     let pingChart;
 
-    // watch(
-    //   [createDate, pings],
-    //   ([createDate, pings], [prevCreateDate, prevPings]) => {
-    //     console.log("inside ping chart watcher");
-
     onMounted(() => {
-      let end, maxTick, minTick, playsByDay, start, x, y, yMax;
+      watch(
+        [createDate, pings],
+        ([createDate, pings], [prevCreateDate, prevPings]) => {
+          let end, maxTick, minTick, playsByDay, start, x, y, yMax;
 
-      function renderChart() {
-        if (typeof pingChart !== "undefined") pingChart.destroy();
-        start = max([
-          zonedTimeToUtc(
-            addDays(Date.now(), -30),
-            Intl.DateTimeFormat().resolvedOptions().timeZone
-          ),
-          zonedTimeToUtc(
-            parseISO(createDate.value),
-            Intl.DateTimeFormat().resolvedOptions().timeZone
-          ),
-        ]);
+          function renderChart() {
+            if (typeof pingChart !== "undefined") pingChart.destroy();
+            start = max([
+              zonedTimeToUtc(
+                addDays(Date.now(), -30),
+                Intl.DateTimeFormat().resolvedOptions().timeZone
+              ),
+              zonedTimeToUtc(
+                parseISO(createDate),
+                Intl.DateTimeFormat().resolvedOptions().timeZone
+              ),
+            ]);
 
-        end = zonedTimeToUtc(Date.now(), Intl.DateTimeFormat().resolvedOptions().timeZone);
-        playsByDay = pings.value.reduce((accum, i) => {
-          accum[i.day] = i.plays;
-          return accum;
-        }, []);
-        x = eachDayOfInterval({
-          start: utcToZonedTime(start, "UTC"),
-          end: utcToZonedTime(end, "UTC"),
-        }).map((day) => format(day, "MM/dd/yyyy"));
+            end = zonedTimeToUtc(Date.now(), Intl.DateTimeFormat().resolvedOptions().timeZone);
+            playsByDay = pings.reduce((accum, i) => {
+              accum[i.day] = i.plays;
+              return accum;
+            }, []);
+            x = eachDayOfInterval({
+              start: utcToZonedTime(start, "UTC"),
+              end: utcToZonedTime(end, "UTC"),
+            }).map((day) => format(day, "MM/dd/yyyy"));
 
-        y = x.map((day) => playsByDay[day] || 0);
-        yMax = Math.max(...y);
-        minTick = format(addDays(utcToZonedTime(start, "UTC"), -1), "MM/dd/yyyy");
-        maxTick = format(addDays(utcToZonedTime(end, "UTC"), 1), "MM/dd/yyyy");
-        pingChart = new Chart(canvasRef.value, {
-          type: "bar",
-          data: {
-            labels: x,
-            datasets: [
-              {
-                label: "Plays",
-                data: y,
-                barThickness: "flex",
-                maxBarThickness: 40,
-                minBarLength: 2,
-                backgroundColor: "rgba(170, 84, 255, .15)",
-                borderColor: "rgba(113, 28, 255, .75)",
-                borderWidth: 1,
-                hoverBackgroundColor: "rgba(113, 28, 255, 1.0)",
-                hoverBorderWidth: 0,
+            y = x.map((day) => playsByDay[day] || 0);
+            yMax = Math.max(...y);
+            minTick = format(addDays(utcToZonedTime(start, "UTC"), -1), "MM/dd/yyyy");
+            maxTick = format(addDays(utcToZonedTime(end, "UTC"), 1), "MM/dd/yyyy");
+            pingChart = new Chart(canvasRef.value, {
+              type: "bar",
+              data: {
+                labels: x,
+                datasets: [
+                  {
+                    label: "Plays",
+                    data: y,
+                    barThickness: "flex",
+                    maxBarThickness: 40,
+                    minBarLength: 2,
+                    backgroundColor: "rgba(170, 84, 255, .15)",
+                    borderColor: "rgba(113, 28, 255, .75)",
+                    borderWidth: 1,
+                    hoverBackgroundColor: "rgba(113, 28, 255, 1.0)",
+                    hoverBorderWidth: 0,
+                  },
+                ],
               },
-            ],
-          },
-          options: {
-            responsive: true,
-            title: {
-              display: true,
-              fontSize: 18,
-              fontStyle: "normal",
-              fontFamily: "'marcher-medium', sans-serif",
-              lineHeight: 1.3,
-              position: "top",
-              text: "Daily Activity",
-            },
-            tooltips: {
-              callbacks: {
-                title: function (tooltipItem) {
-                  return format(
-                    zonedTimeToUtc(
-                      parse(tooltipItem[0].xLabel, "MM/dd/yyyy", new Date()),
-                      Intl.DateTimeFormat().resolvedOptions().timeZone
-                    ),
-                    "EEE, LLL do"
-                  );
+              options: {
+                responsive: true,
+                title: {
+                  display: true,
+                  fontSize: 18,
+                  fontStyle: "normal",
+                  fontFamily: "'marcher-medium', sans-serif",
+                  lineHeight: 1.3,
+                  position: "top",
+                  text: "Daily Activity",
                 },
-              },
-            },
-            legend: {
-              display: true,
-            },
-            animation: {
-              easing: "easeInQuart",
-            },
-            scales: {
-              xAxes: [
-                {
-                  gridLines: {
-                    offsetGridLines: false,
-                  },
-                  type: "time",
-                  ticks: {
-                    fontSize: 12,
-                    max: maxTick,
-                    maxRotation: 180,
-                    maxTicksLimit: 5,
-                    min: minTick,
-                    minRotation: 0,
-                  },
-                  time: {
-                    unit: "week",
-                    parser: "MM/DD/YYYY",
-                    isoWeekday: true,
-                    displayFormats: {
-                      week: "ddd, MMM D",
+                tooltips: {
+                  callbacks: {
+                    title: function (tooltipItem) {
+                      return format(
+                        zonedTimeToUtc(
+                          parse(tooltipItem[0].xLabel, "MM/dd/yyyy", new Date()),
+                          Intl.DateTimeFormat().resolvedOptions().timeZone
+                        ),
+                        "EEE, LLL do"
+                      );
                     },
                   },
-                  scaleLabel: {
-                    display: true,
-                    labelString: "Date (GMT/UTC)",
-                  },
                 },
-              ],
-              yAxes: [
-                {
-                  type: "linear",
-                  ticks: {
-                    beginAtZero: true,
-                    fontSize: 12,
-                    maxTicksLimit: 5,
-                    precision: 0,
-                    suggestedMax: Math.max(yMax + yMax / 5, 10),
-                  },
+                legend: {
+                  display: true,
                 },
-              ],
-            },
-          },
-        });
-      }
-      renderChart();
+                animation: {
+                  easing: "easeInQuart",
+                },
+                scales: {
+                  xAxes: [
+                    {
+                      gridLines: {
+                        offsetGridLines: false,
+                      },
+                      type: "time",
+                      ticks: {
+                        fontSize: 12,
+                        max: maxTick,
+                        maxRotation: 180,
+                        maxTicksLimit: 5,
+                        min: minTick,
+                        minRotation: 0,
+                      },
+                      time: {
+                        unit: "week",
+                        parser: "MM/DD/YYYY",
+                        isoWeekday: true,
+                        displayFormats: {
+                          week: "ddd, MMM D",
+                        },
+                      },
+                      scaleLabel: {
+                        display: true,
+                        labelString: "Date (GMT/UTC)",
+                      },
+                    },
+                  ],
+                  yAxes: [
+                    {
+                      type: "linear",
+                      ticks: {
+                        beginAtZero: true,
+                        fontSize: 12,
+                        maxTicksLimit: 5,
+                        precision: 0,
+                        suggestedMax: Math.max(yMax + yMax / 5, 10),
+                      },
+                    },
+                  ],
+                },
+              },
+            });
+          }
+          renderChart();
+          // });
+        },
+        { immediate: true }
+      );
     });
-
-    //   ,
-    //   { immediate: true }
-    // );
 
     return {
       canvasRef,
--- client/src/views/MatchStats.vue
+++ client/src/views/MatchStats.vue
@@ -1,20 +1,28 @@
 <template>
-  <div>
-    <input
-      :disabled="loading"
-      type="button"
-      :value="loading ? 'loading...' : 'Refresh'"
-      @click.prevent="refreshStats()"
-    />
-    <h1>Match Stats</h1>
-    <h1><label>Match Id</label>{{ matchId }}</h1>
+  <div class="page-wrapper">
+    <!-- <h1><label>Match Id</label>{{ matchId }}</h1>
     <pre><label>Stats</label>{{ JSON.stringify(stats, null, 4) }}</pre>
     <h1><label>Error</label>{{ error }}</h1>
     <h1><label>Failed</label>{{ failed }}</h1>
     <h1><label>Initialized</label>{{ initialized }}</h1>
     <h1><label>Loaded</label>{{ loaded }}</h1>
-    <h1><label>Loading</label>{{ loading }}</h1>
-    <ping-chart v-if="loaded" :create-date="stats.createDate" :pings="stats.pings" />
+    <h1><label>Loading</label>{{ loading }}</h1> -->
+    <div class="container">
+      <input
+        :disabled="loading"
+        type="button"
+        :value="loading ? 'loading...' : 'Refresh'"
+        @click.prevent="refreshStats()"
+      />
+      <h1>Match Stats</h1>
+      <div class="ui-grid">
+        <div class="ui-grid__row">
+          <div class="ui-grid__column">
+            <ping-chart v-if="!!stats" :create-date="stats.createDate" :pings="stats.pings" />
+          </div>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -59,4 +67,52 @@ export default {
 };
 </script>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+.page-wrapper {
+  position: relative;
+  margin-top: 1rem;
+  margin: 0 auto;
+  max-width: 960px;
+}
+
+.container {
+  padding: 1rem;
+}
+
+.ui-grid {
+  $grid: &;
+  $row: #{$grid}__row;
+  $column: #{$grid}__column;
+  display: flex;
+  flex-direction: row;
+  flex-wrap: wrap;
+  padding: 0;
+  align-items: stretch;
+  margin: -1rem;
+
+  &__row {
+    position: relative;
+    display: flex;
+    flex-direction: row;
+    flex-wrap: wrap;
+    justify-content: inherit;
+    align-items: stretch;
+    width: 100% !important;
+    padding: 0;
+    padding-top: 1rem;
+    padding-bottom: 1rem;
+  }
+  &__column {
+  }
+  @at-root #{$grid} > #{$row} > #{$column} {
+    position: relative;
+    display: inline-block;
+    vertical-align: top;
+    padding-left: 1rem;
+    padding-right: 1rem;
+    &:only-child {
+      width: 100%;
+    }
+  }
+}
+</style>
