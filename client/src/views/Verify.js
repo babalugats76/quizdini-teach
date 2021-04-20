9539b93c8fbfb2c5a4518ffa3e0dc25362c4ca7e
James Colestock <james@colestock.com>
2021-04-19 18:46:50 -0600

    move verify changes

--- client/src/views/Verify.js
+++ client/src/views/Verify.js
@@ -1,5 +1,12 @@
+import { onMounted } from "vue";
 import { useVerify } from "@hooks";
 export default {
-  setup: () => useVerify(),
+  setup: () => {
+    const { verify } = useVerify();
+    onMounted(() => {
+      console.log("mounted...executing verify");
+      verify();
+    });
+  },
   render: () => null,
 };
